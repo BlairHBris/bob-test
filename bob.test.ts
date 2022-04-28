f690c4c3175fdb0e2d4813e4a2e2080d13bf43bb
Blair Bristow <blairhbristow@gmail.com>
2022-04-28 15:47:06 -0400

    Add tests

--- bob.test.ts
+++ bob.test.ts
@@ -1,1 +1,28 @@
 import { hey } from "./bob";
+
+describe("hey Function", () => {
+    it("simple statement", () => {
+        const result = hey('Hi.')
+        expect(result).toBe('Whatever.')
+    })
+    
+    it('complex statement', () => {
+        const result = hey("My name is Bob.")
+        expect(result).toBe('Whatever.')
+    })
+
+    it("question", () => {
+        const result = hey("Where you at?")
+        expect(result).toBe('Sure.')
+    })
+
+    it("yelling", () => {
+        const result = hey("I AM CRAZY!")
+        expect(result).toBe('Whoa, chill out!')
+    })
+
+    it("silence", () => {
+        const result = hey("")
+        expect(result).toBe("Fine. Be that way!")
+    })
+})
